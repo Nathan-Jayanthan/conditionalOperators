f93de9c37bd305a128027fa65517665587c983e4
nathan-jayanthan <nathan.jayanthan@yahoo.com>
2020-06-29 21:09:31 -0700

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int age = 21;
+
+        System.out.println(age > 50 ? "old" : "young");
+    }
+}
